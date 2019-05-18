66a4d141648dae9f0c0871df614231870c892c91
OceanDiaali <ocean.diaali@gmail.com>
2019-05-18 20:50:36 +0100

    first commit

---
+++ TahGit/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TahGit
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(true)]
+    public partial class MainPage : ContentPage
+    {
+        //string amountVal;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Title = "TahGit";
+
+        }
+
+        void Button_Clicked(object sender, EventArgs e) {
+
+            var amountVal = Amount.Text;
+            var timeVal = Duration.Text;
+
+            Navigation.PushAsync(new TahgitPage(amountVal, timeVal));
+        }
+
+    }
+}
---
+++ TahGit/TahgitPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+//using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace TahGit
+{
+    public partial class TahgitPage : ContentPage
+    {
+        public string AmtValue { get; }
+        public string TimeValue { get; }
+        public string RemainderValue { get; set; }
+
+
+        double money, period, calc;
+        string result;
+
+        public TahgitPage(string aVal, string tVal)
+        {
+
+            AmtValue = aVal + " Naira";
+            TimeValue = tVal + " days";
+
+            money = Convert.ToDouble(aVal);
+            period = Convert.ToDouble(tVal);
+
+            calc = (money / period);
+            result = Math.Floor(calc).ToString();
+            //result =  calc.ToString("G2");
+
+            RemainderValue = "You need to save " + result + " Naira everyday";
+
+            BindingContext = this;
+            InitializeComponent();
+            Title = "Details";
+
+            //region toolbar
+            ToolbarItem tbi = null;
+
+                tbi = new ToolbarItem("+", "plus",  () =>
+                {
+
+                }, 0, 0);
+            
+            ToolbarItems.Add(tbi);
+            //endregion
+        }
+
+        void OnTapGestureRecognizerTapped(object sender, EventArgs e) {
+            HourGlass.RotateTo(360, 2000);
+            HourGlass.Rotation = 0;
+            Calendarium.IsVisible = true;
+        }
+
+    } // class
+} // namespace
